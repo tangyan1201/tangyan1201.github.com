bc904990d6522903cd44d3ab4a17f3d000fb6a65
unknown <1203801316@qq.com>
2016-01-21 17:03:21 +0800

    2016,1,21

--- .idea/js/indexJs.js
+++ .idea/js/indexJs.js
@@ -12,33 +12,33 @@ window.onload=function(){
 	
 	//首页动画
 
-//(function(){
-//		var opc=document.getElementById('j-ani-opc');
-//		var oImg=document.getElementById('j-img');
-//		var oTxt=document.getElementById('j-ani-txt');
-//		
-//		move(oImg,{opacity:1,filter:'alpha(opacity:100)'},{
-//					duration:1500,
-//					complete:function(){
-//						move(oTxt,{opacity:1,filter:'alpha(opacity:100)'},{duration:1000})
-//							
-//					}
-//			
-//		})
-//		
-//		setTimeout(function(){
-//			move(opc,{opacity:0.3,filter:'alpha(opacity:30)'},{duration:2000,
-//				complete:function(){
-//					opc.style.display='none';
-//				}
-//			})
-//		},4500);
-//		
-//		opc.onclick=function(){
-//			opc.style.display='none';
-//		};
-//
-//})();
+(function(){
+		var opc=document.getElementById('j-ani-opc');
+		var oImg=document.getElementById('j-img');
+		var oTxt=document.getElementById('j-ani-txt');
+		
+		move(oImg,{opacity:1,filter:'alpha(opacity:100)'},{
+					duration:1500,
+					complete:function(){
+						move(oTxt,{opacity:1,filter:'alpha(opacity:100)'},{duration:1000})
+							
+					}
+			
+		})
+		
+		setTimeout(function(){
+			move(opc,{opacity:0.3,filter:'alpha(opacity:30)'},{duration:2000,
+				complete:function(){
+					opc.style.display='none';
+				}
+			})
+		},4500);
+		
+		opc.onclick=function(){
+			opc.style.display='none';
+		};
+
+})();
 
 
 	/*
